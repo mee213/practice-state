a92de25137b731a28860e9a71cdcf328da35ec71
Marlo Wendell <mee213@gmail.com>
2019-03-25 16:53:29 -0500

    solution

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -4,19 +4,42 @@ import Star from './Star';
 class StarRating extends Component {
 
   // Initialize a 'rating' state
-
+  state = {
+    rating: 0
+  };
 
   // Write a function that returns 5 Star components
+  //use loop and push method to add stars to an array based on the number value
+  //then, render the stars to the dom
 
+  showStars = () => {
+    let stars = [];
+    let maxRating = 5;
 
-  // Write an event handler that updates the rating state.
-  // Pass the function to a Star component via props
+    for (let i=0; i<maxRating; i+=1) {
+      stars.push(<Star 
+        isSelected={this.state.rating > i}
+        key={i}
+        setRating={ () => this.handleSetRating(i+1) }
+      />);
+    };
 
+    return stars;
+  };
+
+  // Write an event handler that updates the rating state based on the star a user clicks.
+  // Pass the function to a Star component via props, and called when a user clicks on a star
+  handleSetRating = (rating) => {
+    this.setState({
+      rating
+    });
+  };
 
   render() {
     return (
       <ul className="course--stars">
-        {/* Render the Star components */}
+        {this.showStars()}
+        
       </ul>
     );
   }
